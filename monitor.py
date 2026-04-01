79c8fa5f3becce36e8a3eeebe9a794d89dfa5786
kolasdocument <kolasdocument@gmail.com>
2026-04-01 13:19:17 +0900

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -21,11 +21,22 @@ LIST_URL = "https://www.knab.go.kr/inf/bbs/lawrecsroom/LawRecsRoomList.do"
 
 # 헤더를 엑셀(MSXML) 수준으로 단순화하면서도 브라우저처럼 보이게 설정
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/122.0.0.0',
-    'Content-Type': 'application/x-www-form-urlencoded', # 엑셀과 동일 설정
-    'Accept': '*/*',
+   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br', # 압축 전송 허용
+    'Accept-Language': 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7',
+    'Cache-Control': 'max-age=0',
+    'Content-Type': 'application/x-www-form-urlencoded',
     'Origin': 'https://www.knab.go.kr',
-    'Referer': BASE_URL
+    'Referer': 'https://www.knab.go.kr/inf/bbs/lawrecsroom/LawRecsRoom.do',
+    'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Google Chrome";v="122"',
+    'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua-Platform': '"Windows"',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1'
 }
 
 updated_docs = []
